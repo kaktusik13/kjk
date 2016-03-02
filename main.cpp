8b65307f7ad3a16258e7de4f7e69f90750c720a7
kaktusik13 <kskulas1@gmail.com>
2016-03-02 10:43:25 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+class ZOO
+{
+ public:
+    char*gatunek;
+    int masa;   // masa-m
+    virtual void głos() = 0;
+
+};
+class koza :public ZOO {
+    public głos();
+    cout << głos << "bee"<< endl};
+ZOO {protected vector <Zwierze*>z;
+public :dodaj <Zwierze*>;
+        usuń <Zwierze*>;
+};
+
+cout<< Zwierze.dodaj (new koza ());
