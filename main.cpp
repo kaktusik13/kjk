6a36e6c18fd49602c75f866a8d1e00598b3867ca
kaktusik13 <kskulas1@gmail.com>
2016-03-16 10:50:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,139 @@
-class ZOO
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class Zwierze
 {
- public:
-    char*gatunek;
-    int masa;   // masa-m
-    virtual void głos() = 0;
+protected:
+   int8_t masa;
+public:
+    virtual void daj_glos() = 0; 
+
+
 
 };
-class koza :public ZOO {
-    public głos();
-    cout << głos << "bee"<< endl};
-ZOO {protected vector <Zwierze*>z;
-public :dodaj <Zwierze*>;
-        usuń <Zwierze*>;
+
+
+class Krowa : public Zwierze
+{
+public:
+    void daj_glos()
+    {
+        cout << "Muu!\n";
+    }
+
 };
 
-cout<< Zwierze.dodaj (new koza ());
+class Koza : public Zwierze
+{
+public:
+    void daj_glos()
+    {
+        cout << "Beeeeee!\n";
+    }
+    int8_t masa()
+    {
+        cout << "100kg\n";
+    }
+};
+
+
+class Kon :public Zwierze
+
+{
+public:
+    void daj_glos()
+    {
+        cout <<"Ihaa!\n";
+    }
+
+
+};
+
+
+class Zoo
+{
+protected:
+    vector <Zwierze*> v;
+
+public:
+    Zoo(){
+    srand(time(NULL));
+    v.push_back(new Krowa());
+    v.push_back(new Koza());
+    v.push_back(new Kon());
+
+}
+void glosy() {
+    for (int i=0; i<v.size(); ++i)
+        v.at(i) -> daj_glos();
+}
+};
+
+
+class Obserwator
+{
+    public:
+    virtual void update() =0;
+};
+
+
+
+class Obserwowany {
+protected:
+    std::list <Obserwator*> obserwatorzy;
+public:
+    void dodaj(Obserwator *Zwierze) {
+        obserwatorzy.push_back (Zwierze);
+    }
+    void usun(Obserwator *Zwierze) {
+        obserwatorzy.remove (Zwierze);
+    }
+
+    void powiadom () {
+        std::list<Obserwator *>::iterator it;
+        for (it = obserwatorzy.begin(); it != obserwatorzy.end(); it++) {
+            (*it)->update ();
+        }
+    }
+};
+
+
+
+class ObserwowanyKon : public Obserwowany, public Kon {
+    std::string stan;
+public:
+    ObserwowanyKon ( void daj_glos()) : Kon(daj_glos) {
+    }
+
+    std::string pobierzStan () {
+        return stan;
+    }
+
+    void ustawStan (const std::string& s) {
+        stan = s;
+        std::cout << "Stan: " << stan << std::endl;
+        powiadom();
+    }
+};
+
+
+int main()
+{
+    //srand (time(NULL));
+    //Zwierze *v.at(rand()%v.size()) -> zabij();
+
+
+
+
+    Zoo zoo;
+    zoo.glosy();
+
+
+
+
+    return 0;
