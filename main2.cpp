2daec6eec964c170dc7d5c2b508486f119b07235
kaktusik13 <kskulas1@gmail.com>
2016-05-25 09:56:46 +0200

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <cstdio>
+#include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -17,28 +20,82 @@ class Right
 {
     string wl_alarmu();
     string wyl_alarmu();
-    public: char powiadomienia();
+
+    char powiadomienia()
+    {
+        int data[10];
+        int godzina[10];
+        string tresc[100];
+    }
+
+    public:
+    void wyslij_powiadomienie(Uzytkownik*)
+    {
+        vector <Uzytkownik*> itertion:: it;
+        for (it = Uzytkownik.begin(); it= Obserwator.endl(); ++i);
+        {
+            powiadomienia();
+        };
+        cout<< "data"<< data<< "godzina"<< godzina<< tresc<< endl;
+    };
 }
 
 class Uzytkownik: public System
 {
-    private: string addRight();
+   public: string addRight();
     char nazwa_uzytkownika;
     vector <Right> rights;
 
 }
 
-class add_new_users : public Right
+class add_delete_new_users : public Right
 {
-     private: string dodaj_uzytkownika();
-     private: string usun_uzytkownika();
+     char nazwa_uzytkownika;
+     vector <Uzytkownik*> Obserwator;
+
+
+     private:
+     if (nazwa_uzytkownika = "ADMIN")
+     {
+         void dodaj_uzytkownika();
+         void usun_uzytkownika ();
+     };
+
+     void dodaj_uzytkownika(Uzytkownik* n)
+    {
+        cin >> nazwa_uzytkownika;
+        cout << nazwa_uzytkownika << "dodaj uzytkownika " << n -> nazwa_uzytkownika << endl;
+    };
+
+
+     void usun_uzytkownika(Uzytkownik* n)
+     {
+        cin >> nazwa_uzytkownika;
+        cout << "usun uzytkownika" << nazwa_uzytkownika <<  n -> nazwa_uzytkownika << endl;
+
+        // for (uint32_t i =0; i< a.size(); **i);
+           // a [i] -> usun_uzytkownika (n);
+     };
+
+
+
 }
 
 class dziennik_aktywnosci : public Right
 {
-    public: char odczyt_czujnikow();
-    public: char odczyt_kamer();
-    public: char odcyt_ostatnich_logowan_do_systemu();
+    public:
+    char odczyt_czujnikow();
+    char odczyt_kamer();
+    char odczyt_ostatnich_logowan_do_systemu();
+    void ostatnie_logowanie ()
+    {
+        int data[10];
+        int godzina[10];
+        char nazwa_uzytkownika;
+        cout << data << godzina << nazwa_uzytkownika << endl;
+    }
+
+
 }
 
 
@@ -66,8 +123,11 @@ class Kamera_wewn: public Kamery
 }
 
 class Czujniki: public Urzadzenia_peryferyjne
+   /*int data[10];
+    int godzina[10];
+    string stan;*/
 {
-
+    //cin.get (data, godzina, stan)
 }
 
 class Czujnik_dymu: public Czujniki {}
@@ -90,10 +150,8 @@ class Panel: public Urzadzenia_peryferyjne
    char  zmiana_hasla();
 
    char odczyt_dziennika_aktywnosci();
-
-
 };
+
 /*int main()
 {
-
 }*/
